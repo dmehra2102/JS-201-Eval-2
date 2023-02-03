13eee4590b3ef1e4e81abab9b072aed17b580d7e
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2023-02-03 11:00:13 +0530

    eval-2 done

--- src/index.js
+++ src/index.js
@@ -1,94 +1,83 @@
 
 // Problem 1. 
+function Animal(name, type) {
+  this.name = name;
+  this.type = type;
+}
 
-class Animal {
-    constructor(name, type) {
-      this.name = name;
-      this.type = type;
-    }
-  
-    makeSound() {
-      return "Animal is making a sound";
-    }
-  }
-  
-  // Level 2
-  class Mammal extends Animal {
-    constructor(name, type, hasFur) {
-      super(name, type);
-      this.hasFur = hasFur;
-    }
-  
-    makeSound() {
+Animal.prototype.makeSound = ()=>{
+  return "Animal is making a sound";
+}
+
+function Mammal(name, type, hasFur) {
+  Animal.call(this,name,type);
+  this.hasFur = hasFur;
+  this.makeSound = ()=>{ 
       return "Mammal is making a sound";
-    }
   }
-  
-  // Level 3
-  class Dog extends Mammal {
-    constructor(name, type, hasFur, breed) {
-      super(name, type, hasFur);
-      this.breed = breed;
-    }
-  
-    makeSound() {
+}
+
+Mammal.prototype = Object.create(Animal.prototype);
+Mammal.prototype.constructor= Mammal;
+
+
+function Dog(name, type, hasFur, breed) {
+  Mammal.call(this,name,type,hasFur);
+  this.breed = breed;
+  this.makeSound = ()=> {
       return "Woof Woof!";
-    }
   }
+}
 
 
-//   Problem 2
+Dog.prototype = Object.create(Mammal.prototype);
+Dog.prototype.constructor = Dog;
+
+
+let d = new Dog("Fido", "dog", true, "Labrador");
+console.log(d.makeSound()); // "Woof Woof!"
+
+let m = new Mammal("Fluffy", "mammal", true);
+console.log(m.makeSound()); // "Mammal is making a sound"
+
+let a = new Animal("Charlie", "animal");
+console.log(a.makeSound()); // "Animal is making a sound"
 
 
 class Character {
-    constructor(name,health,attackPower){
-        this.name = name;
-        this.health= health;
-        this.attackPower = attackPower;
-        this.attack = function(obj){
-            obj.health -= this.attackPower;
-        }
-        this.isAlive = function(){
-            return this.health>0;
-        }
-    }
+  constructor(name,health,attackPower){
+      this.name = name;
+      this.health= health;
+      this.attackPower = attackPower;
+      this.attack = function(obj){
+          obj.health -= this.attackPower;
+      }
+      this.isAlive = function(){
+          return this.health>0;
+      }
+  }
 }
 
 class Warrior extends Character {
-    constructor(name,health,attackPower,weapon,armor){
-        super(name,health,attackPower);
-        this.weapon = weapon;
-        this.armor= armor;
-        this.attack = function(obj){
-            if(weapon==="sword"){
-                obj.health -= this.attackPower + 10;
-            }else{
-                obj.health -= this.attackPower;
-            }
-        }
-        this.defend = function(){
-            this.health += 10;
-        }
-    }
+  constructor(name,health,attackPower,weapon,armor){
+      super(name,health,attackPower);
+      this.weapon = weapon;
+      this.armor= armor;
+      this.attack = function(obj){
+          if(weapon==="sword"){
+              obj.health -= this.attackPower + 10;
+          }else{
+              obj.health -= this.attackPower;
+          }
+      }
+      this.defend = function(){
+          this.health += 10;
+      }
+  }
 }
 
-let character = new Character("John", 100, 20);
-let enemy = new Character("Enemy", 100, 20);
-let warrior = new Warrior("John", 100, 20, "sword", 30);
-
-// console.log(enemy.health); // 100
-// character.attack(enemy);
-// character.attack(enemy);
-// console.log(enemy.health); // 60
-// warrior.attack(enemy);
-// console.log(enemy.health); // 30
-
-// console.log(warrior.health); // 100
-// warrior.defend();
-// console.log(warrior.health); // 110
-
-// console.log(enemy.isAlive()); // true
-// enemy.health = 0;
-// console.log(enemy.isAlive()); // false
+// let character = new Character("John", 100, 20);
+// let enemy = new Character("Enemy", 100, 20);
+// let warrior = new Warrior("John", 100, 20, "sword", 30);
 
 export { Dog, Mammal, Animal, Character, Warrior }
